58eb50da6f9315c32d4914da48598c6d67ab1378
antonielle <marytony0606@hotmail.com>
2016-03-14 13:14:54 -0600

    agregados
    Copie algunos archivos que necesito para crear mis pantallas...

---
+++ js/mayus.js
@@ -0,0 +1,6 @@
+ <script type="text/javascript">
+function upperCase() {
+   var x=document.getElementById("fname").value
+   document.getElementById("fname").value=x.toUpperCase()
+}
+</script>
---
+++ js/popover.js
@@ -0,0 +1,9 @@
+
+//Para la aplicación de los popovers
+<script> 
+                 $(document).ready(function(){
+                 $('[data-toggle="popover"]').popover();   
+                  });
+             </script>
+
+    
