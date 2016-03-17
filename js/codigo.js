b9fc7e34ac9275e7fdae60fbf524d077beed2663
gbrlquiroz <orion_fire_qog@hotmail.com>
2016-03-17 15:57:23 -0600

    ejemplos de ayuda

---
+++ js/codigo.js
@@ -0,0 +1,23 @@
+    $(function () {
+        $('#ok').click(function () {
+            var valido = true;
+            $('input').each(function () {
+                var grupo = $(this).parents('.form-group');
+                var icono = grupo.find('.glyphicon');
+                if (this.checkValidity()) {
+                    grupo.addClass('has-success').removeClass('has-error');
+                    icono.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+                } else {
+                    grupo.addClass('has-error').removeClass('has-success');
+                    icono.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+                    valido = false;
+                }
+            });
+
+            if (valido) {
+                //$('#miModal').modal('hide');
+                $('#success-alert').removeClass('hidden');
+            }
+
+        });
+    });
