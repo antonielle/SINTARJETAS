a7f33d7e9ab29e3fcca7799177f6a78d621acba4
antonielle <marytony0606@hotmail.com>
2016-03-17 15:54:33 -0600

    DEPURACION

--- js/mayus.js
+++
@@ -1,6 +0,0 @@
- <script type="text/javascript">
-function upperCase() {
-   var x=document.getElementById("fname").value
-   document.getElementById("fname").value=x.toUpperCase()
-}
-</script>
--- js/popover.js
+++
@@ -1,9 +0,0 @@
-
-//Para la aplicación de los popovers
-<script> 
-                 $(document).ready(function(){
-                 $('[data-toggle="popover"]').popover();   
-                  });
-             </script>
-
-    
